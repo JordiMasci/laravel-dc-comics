de2da302ed9a0ce3697d51978c64256d6b985ab3
JordiMasci <133970995+JordiMasci@users.noreply.github.com>
2023-10-18 18:27:46 +0200

    feat: tabella in index

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -6,21 +6,23 @@
 
         <table class="table table-striped">
             <thead>
-              <tr>
-                <th scope="col">#</th>
-                <th scope="col">First</th>
-                <th scope="col">Last</th>
-                <th scope="col">Handle</th>
-              </tr>
+                <tr>
+                    <th scope="col">Id</th>
+                    <th scope="col">Titolo</th>
+                    <th scope="col">Prezzo</th>
+                    <th scope="col">data</th>
+                </tr>
             </thead>
             <tbody>
-              <tr>
-                <th scope="row">1</th>
-                <td>Mark</td>
-                <td>Otto</td>
-                <td></td>
-              </tr>
+                @foreach ($comics as $comic)
+                    <tr>
+                        <th scope="row">{{$comic->id}}</th>
+                        <th scope="col">{{$comic->title}}</th>
+                        <th scope="col">{{$comic->price}}</th>
+                        <th scope="col">{{$comic->sale_date}}</th>
+                    </tr>
+                @endforeach
             </tbody>
-          </table>
+        </table>
     </div>
 @endsection
