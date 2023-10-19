d4786e05ed1359c9df21a8e58bde1740c03c0b46
JordiMasci <133970995+JordiMasci@users.noreply.github.com>
2023-10-19 17:28:39 +0200

    feat: modal -> senza form

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -46,15 +46,43 @@
                                 <i class="fa-solid fa-eye"></i>
                             </a>
 
-                            <form action="{{ route('comics.destroy', $comic) }}" class="mx-2" method="POST">
+                            <!-- Button trigger modal -->
+                            <button type="button"  data-bs-toggle="modal"
+                                data-bs-target="#exampleModal">
+                                <i class="fa-solid fa-trash"></i>
+                            </button>
+
+                            <!-- Modal -->
+                            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel"
+                                aria-hidden="true">
+                                <div class="modal-dialog">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                                            <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                                aria-label="Close"></button>
+                                        </div>
+                                        <div class="modal-body">
+                                            ...
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-secondary"
+                                                data-bs-dismiss="modal">Close</button>
+                                            <button type="button" class="btn btn-primary">Save changes</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+
+                            {{-- <form action="{{ route('comics.destroy', $comic) }}" class="mx-2" method="POST">
                                 @csrf
                                 @method('DELETE')
 
                                 <button>
-                                    <i class="fa-solid fa-trash"></i>
                                 </button>
 
-                            </form>
+                            </form> --}}
                         </th>
                     </tr>
                 @endforeach
